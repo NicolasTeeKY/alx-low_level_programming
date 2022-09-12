5e618e4dc193720d2505a603a2ffcfebcad418bf
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-12 08:08:00 -0700

    pro-done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
 #!/bin/bash
-gcc
+#include <stdio.h>
+
+/**
+ * Main -entry
+ *
+ * Description : prints exactly 'programming is like
+ * building a multilingual puzzle'
+ *
+ * Return: 0 
+ *
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return(0);
+}
