dc22a8de2c8a6a1746b44f912dd03893115eb8b1
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-13 14:21:42 -0700

    undone

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry
+ *
+ * Description: check the code if c is a letter
+ * lowercase or uppercase
+ *
+ * @c: is the ketter to check
+ *
+ * Return: 1 if c is an alphabet, otherwise 0
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c >= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
