f84e1bfd9f357718cfabe9268c651dc0bdbe349e
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-14 14:22:53 -0700

    undone

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - entry
+ *
+ * Description: adds two integers
+ * @x: plus y
+ * @y: plus x
+ *
+ * Return: the sum of two intergers
+ *
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+
+}
