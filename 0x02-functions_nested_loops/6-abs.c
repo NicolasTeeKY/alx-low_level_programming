e50dad4e15e06e0f75a9a87839a31e78d63d539c
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-13 20:32:43 -0700

    halfdone

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - main entry
+ *
+ * Descriptio: computes the absolute value of an int
+ *
+ * @n: the int to check
+ *
+ * Return: absolute value
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		ruturn(n);
+	}
+	return (-n);
+}
