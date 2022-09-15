5ebe24ab06eab3e4713f1eb243dcbddba17a5ef5
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-15 07:59:42 -0700

    pro-done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /*
- * positive_or_negative - Entry
- *
- * Description: tests function that prints
+ * main - tests function that prints
  * if integer is positive or negative
  *
- * Return: Always 0
+ * Return: 0
  */
-positive_or_negative(int i)
+
+int main(void)
 {
 	int i;
 
