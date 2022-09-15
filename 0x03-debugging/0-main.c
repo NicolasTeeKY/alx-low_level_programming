bd19b81d75a29fc67e4b16071c5a2fd882f919f2
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-15 02:54:29 -0700

    undone

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * main - tests function that prints if integer is
  * positive or negative
  *
@@ -10,9 +9,11 @@
 
 int main(void)
 {
-	int i;
+	int i
+		;
 	i = 98;
+
 	positive_or_negative(i);
 
- return (0);
+	return (0);
 }
