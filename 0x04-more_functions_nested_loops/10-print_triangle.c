99c174a9f27824cb8118f25226b277483aeadbf9
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-15 18:45:52 -0700

    prodone

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,3 +7,28 @@
  */
 
 void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, int j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+		}
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+
+}
