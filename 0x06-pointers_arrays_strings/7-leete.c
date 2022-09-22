009128fd31baae47ae7063d4283cd21460ee1428
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-09-22 15:50:21 -0700

    done

--- 0x06-pointers_arrays_strings/7-leete.c
+++ 0x06-pointers_arrays_strings/7-leete.c
@@ -2,4 +2,27 @@
 
 /**
  * leete - encodes a string to 1337
- * @a: a string to encode
+ * @k: a string to encode
+ *
+ * Return: the results
+ */
+char *leete(char *k)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; k[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (k[i] == a[j])
+			{
+				k[i] = b[j];
+			}
+		}
+	}
+
+	return (k);
+}
