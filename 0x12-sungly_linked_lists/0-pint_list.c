06e652c8475802fb20a4c036144229b66ce95101
TeeKY <111075057+NicolasTeeKY@users.noreply.github.com>
2022-10-21 00:16:35 -0700

    print lists

---
+++ 0x12-sungly_linked_lists/0-pint_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * print_list - prints all elements of list_t list
+ * @h: singly linked list
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t k;
+
+	k = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		k++;
+	}
+	return (k);
+}
---
+++ 0x12-sungly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0x12-singly_linked_lists - this is the task about singly linked lists in c.
